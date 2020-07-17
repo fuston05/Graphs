ba97a6e3adee60678a1993c06b7fc9a5b0cb9307
fuston05 <fuston05@yahoo.com>
2020-07-17 18:55:58 -0400

    passses 1st and 2nd maps"

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -8,60 +8,6 @@ from ast import literal_eval
 # Load world
 world = World()
 
-
-class Queue():
-    def __init__(self):
-        self.queue = []
-
-    def enqueue(self, value):
-        self.queue.append(value)
-
-    def dequeue(self):
-        if self.size() > 0:
-            return self.queue.pop(0)
-        else:
-            return None
-
-    def size(self):
-        return len(self.queue)
-
-
-def randomDir():
-    randomDir = random.randrange(0, 3)
-    if randomDir == 0:
-        direction = 'n'
-    elif randomDir == 1:
-        direction = 's'
-    elif randomDir == 2:
-        direction = 'e'
-    elif randomDir == 3:
-        direction = 'w'
-    print('rand dir: ', direction)
-    return direction
-
-
-def getDirectionOpposite(dir):
-    if dir == 'n':
-        opposite = 's'
-    elif dir == 's':
-        opposite = 'n'
-    elif dir == 'e':
-        opposite = 'w'
-    elif dir == 'w':
-        opposite = 'e'
-    return opposite
-
-
-def chooseDirection():
-    exits = player.current_room.get_exits()
-    for e in exits:
-        if traversal_graph[player.current_room.id][e] == '?':
-            direction = e
-            return direction
-    # id all exites have been explored
-    return False
-
-
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
 map_file = "maps/test_cross.txt"
@@ -78,47 +24,161 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
-# Fill this out with directions to walk
-# traversal_path = ['n', 'n']
-traversal_path = []
-traversal_graph = {}
+# *********************************************************** #
+# *********************************************************** #
 
+# ************** functions ***************
+# not currently using randomDir ******
+def randomDir(camefrom, exits):
+    randomDir = random.randrange(0, exits)  # exclusive
+    if randomDir == 0:
+        direction = 'n'
+    elif randomDir == 1:
+        direction = 's'
+    elif randomDir == 2:
+        direction = 'e'
+    elif randomDir == 3:
+        direction = 'w'
+    if cameFrom != direction:
+        print('rand dir: ', direction)
+        return direction
+    return randomDir(cameFrom, exits)
+
+def chooseDir(cameFrom):
+    possibleDirs= player.current_room.get_exits()
+    for dir in possibleDirs:
+        if traversal_graph[curRoom.id][dir] == '?':
+            # check if it's the way we came or not
+            # cameFrom is a list of directions we've been from curRoom
+            if cameFrom != dir:
+                return dir
+    return False
+
+def getDirectionOpposite(dir):
+    if dir == 'n':
+        opposite = 's'
+    elif dir == 's':
+        opposite = 'n'
+    elif dir == 'e':
+        opposite = 'w'
+    elif dir == 'w':
+        opposite = 'e'
+    return opposite
+
+# def movePlayer(direction, curRoom, prevRoom):
+#     # move the player in the chosen direction, and show room info
+#     player.travel(direction, show_rooms=True)
+#     if traversal_graph[prevRoom.id][(direction)] == '?':
+#         traversal_graph[prevRoom.id][direction]= curRoom.id
+#     # update our tracking vars for each move
+#     traversal_path.append(direction)
+#     # curRoom= player.current_room
+#  *************************************
+traversal_graph={}
+# build the traversal_graph as a parallel to track visited rooms and their dir's
+for i in range(len(room_graph)):
+    traversal_graph[i]= {'n': '?', 's': '?', 'e': '?', 'w': '?'}
+
+traversal_path = []
+
+# track visited room id's
+visited = set()
+
+# if we're back tracking
+backTracking= False
+
+# players starting current room object
+curRoom = player.current_room
+prevRoom= curRoom
+print('curRoom: ', curRoom)
+# current room exits list
+curExits = curRoom.get_exits()
+
+# track most recent dir we came from: 'n', 's', etc
+cameFrom= None
+count= 20
+# while len(visited) != len(room_graph):
+# temporary loop counter during testing to avoid the inf loop
+while count > 0:
+    # current room exits list
+    curExits = curRoom.get_exits()
+
+    # figure out a direction to go
+    direction = chooseDir(cameFrom)
+    # returns False if no directions from current room 
+    # that are NOT 'cameFrom'
+    if direction == False:
+        # then we're at a dead end
+        print('dead-end, Time to turn around')
+        backTracking= True
+        direction= getDirectionOpposite(traversal_path[-1])
+
+    # is there a room in that direction?
+    if player.current_room.get_room_in_direction(direction):
+
+        # current 'came from' dir: 'n', 's', etc
+        cameFrom= (getDirectionOpposite(direction))
+        # track prev room so we can update traversal_graph
+        prevRoom= player.current_room
+        print('prevRoom: ', prevRoom.id)
+
+        if traversal_graph[curRoom.id][direction] == '?':
+            player.travel(direction, show_rooms=True)
+            curRoom= player.current_room
+            traversal_graph[prevRoom.id][direction]= curRoom.id
+            print('current room: ', curRoom.id)
+
+        # update our tracking vars for each move
+        traversal_path.append(direction)
+    else:
+        print('no room in that direction')
+    # used for temporary loop counter for testing to avoid the inf loop
+    count-= 1
+
+
+
+#  ***************
+# test prints:
+print('traversal_path: ', traversal_path)
+print('')
+print('visited: ', visited)
+print('')
+print('traversal_graph: ', traversal_graph)
+
+
+# OLD CODE ***********************************
 # generate a parallell of the room graph, for -
 # traversal of unexplored rooms that have a '?'
 # populate this as we go
-for i in range(len(room_graph)):
-    traversal_graph[i] = {'n': '?', 's': '?', 'w': '?', 'e': '?'}
+# for i in range(len(room_graph)):
+#     traversal_graph[i] = {'n': '?', 's': '?', 'w': '?', 'e': '?'}
 
-q = Queue()
-q.enqueue([player.current_room.id])
-# Create a Set to store visited rooms
-visited = set()
+# q = Queue()
+# q.enqueue({player.current_room.id: traversal_graph[player.current_room.id]})
 
-while q.size() > 0:
-    # Dequeue the first PATH
-    path = q.dequeue()
-    # Grab the last vertex from the PATH
-    lastRoom = path[-1]
-    print('last room: ', lastRoom)
-    # If that vertex has not been visited...
-    if lastRoom not in visited:
-       
-        visited.add(lastRoom)
-        # Then add A PATH TO its neighbors to the back of the queue
-        for neighbor in player.current_room.get_exits():
-            # COPY THE PATH
-            tempPath = list(path)
-            # APPEND THE NEIGHOR TO THE BACK
-            tempPath.append(neighbor)
-            q.enqueue(tempPath)
-
-
+# # Create a Set to store visited rooms
+# visited = set()
+# print('Q: ', q)
+# while q.size() > 0:
+# # Dequeue the first PATH
+# path = q.dequeue()
+# # Grab the last vertex from the PATH
+# lastRoom = path[-1]
+# print('last room: ', lastRoom)
+# # If that vertex has not been visited...
+# if lastRoom not in visited:
+#     visited.add(lastRoom)
+#     # Then add A PATH TO its neighbors to the back of the queue
+#     for neighbor in player.current_room.get_exits():
+#         # COPY THE PATH
+#         tempPath = list(path)
+#         # APPEND THE NEIGHOR TO THE BACK
+#         tempPath.append(neighbor)
+#         q.enqueue(tempPath)
 
 
-
-
-print('traversal graph: ', traversal_graph)
-print('traversal_path', traversal_path)
+# print('traversal graph: ', traversal_graph)
+# print('traversal_path', traversal_path)
 # print('randomDir: ', randomDir())
 # print('# of rooms: ', len(room_graph))
 # print('room id: ', player.current_room.id)
@@ -143,7 +203,6 @@ print('traversal_path', traversal_path)
 # print('get exits', player.current_room.get_exits())
 
 
-
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
@@ -154,11 +213,11 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
