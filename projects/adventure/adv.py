21db00bfa98473bf4d1eabe4c9a40917b5b1751c
fuston05 <fuston05@yahoo.com>
2020-07-17 10:21:36 -0400

    restarted from scratch, trying something else"

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -8,41 +8,69 @@ from ast import literal_eval
 # Load world
 world = World()
 
+
 class Queue():
     def __init__(self):
         self.queue = []
+
     def enqueue(self, value):
         self.queue.append(value)
+
     def dequeue(self):
         if self.size() > 0:
             return self.queue.pop(0)
         else:
             return None
+
     def size(self):
         return len(self.queue)
 
-def randomDir(curExits):
-    randomDir= random.randrange(0, curExits)
+
+def randomDir():
+    randomDir = random.randrange(0, 3)
     if randomDir == 0:
-        direction= 'n'
+        direction = 'n'
     elif randomDir == 1:
-        direction= 's'
+        direction = 's'
     elif randomDir == 2:
-        direction= 'e'
+        direction = 'e'
     elif randomDir == 3:
-        direction= 'w'
+        direction = 'w'
     print('rand dir: ', direction)
     return direction
 
+
+def getDirectionOpposite(dir):
+    if dir == 'n':
+        opposite = 's'
+    elif dir == 's':
+        opposite = 'n'
+    elif dir == 'e':
+        opposite = 'w'
+    elif dir == 'w':
+        opposite = 'e'
+    return opposite
+
+
+def chooseDirection():
+    exits = player.current_room.get_exits()
+    for e in exits:
+        if traversal_graph[player.current_room.id][e] == '?':
+            direction = e
+            return direction
+    # id all exites have been explored
+    return False
+
+
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+# map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -53,45 +81,39 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
-traversal_graph= {}
+traversal_graph = {}
 
 # generate a parallell of the room graph, for -
 # traversal of unexplored rooms that have a '?'
 # populate this as we go
 for i in range(len(room_graph)):
-    traversal_graph[i]= {'n': '?', 's': '?', 'w': '?', 'e': '?'}
+    traversal_graph[i] = {'n': '?', 's': '?', 'w': '?', 'e': '?'}
 
-q= Queue()
-q.enqueue(player.current_room.id)
+q = Queue()
+q.enqueue([player.current_room.id])
+# Create a Set to store visited rooms
+visited = set()
 
 while q.size() > 0:
-    curRoom= q.dequeue()
-    prevRoom= curRoom
-    print('curRoom id: ', curRoom)
+    # Dequeue the first PATH
+    path = q.dequeue()
+    # Grab the last vertex from the PATH
+    lastRoom = path[-1]
+    print('last room: ', lastRoom)
+    # If that vertex has not been visited...
+    if lastRoom not in visited:
+       
+        visited.add(lastRoom)
+        # Then add A PATH TO its neighbors to the back of the queue
+        for neighbor in player.current_room.get_exits():
+            # COPY THE PATH
+            tempPath = list(path)
+            # APPEND THE NEIGHOR TO THE BACK
+            tempPath.append(neighbor)
+            q.enqueue(tempPath)
 
-    # get exits
-    exits= player.current_room.get_exits()
-    print('current exits: ', exits)
-    if exits:
-        # if rand direction exists in current room's exits
-        direction= randomDir(len(exits))
-        print('dir: ', direction)
-    #else: if there are no exits: dead-end
-    # walk back
-    # if no exits
-    else: 
-        print('This room is a dead-end')
 
-    if traversal_graph[prevRoom][direction] == '?':
-        # move to new room in new 'direction
-        player.travel(direction, show_rooms= True)
-        # pick an exit, and GO there.. IF it has a '?' as a value for that dir
-        q.enqueue(player.current_room.id)
-        print('new room: ', player.current_room.id)
-        # log this room into path and graph
-        traversal_path.append(direction)
-        traversal_graph[prevRoom][direction]= player.current_room.id
-        
+
 
 
 
